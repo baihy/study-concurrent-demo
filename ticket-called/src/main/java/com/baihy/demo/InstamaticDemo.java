ca56c844f7a2442835fe49a8a3f55b7ebf798eca
huayang.bai <1053091115@qq.com>
2019-08-07 17:21:04 +0800

    并发编程学习demo代码

---
+++ ticket-called/src/main/java/com/baihy/demo/InstamaticDemo.java
@@ -0,0 +1,19 @@
+package com.baihy.demo;
+
+/**
+ * @projectName: study-concurrent-demo
+ * @packageName: com.baihy.demo
+ * @description:  飞机订票系统
+ * @author: huayang.bai
+ * @date: 2019/08/07 16:47
+ */
+public class InstamaticDemo {
+
+    public static void main(String[] args){
+
+        System.out.println("dddddddddddddddd");
+
+    }
+
+
+}
