26f3856c656313cf1cdedae6d7b92b963195d956
huayang.bai <1053091115@qq.com>
2019-08-09 14:44:35 +0800

    并发编程学习demo代码

--- ticket-called/src/main/java/com/baihy/singleton/SingletonDemo.java
+++ ticket-called/src/main/java/com/baihy/singleton/SingletonDemo.java
@@ -24,7 +24,7 @@ public class SingletonDemo {
         // 双重检查确保对象是单例的
         if (null == singletonDemo) {
             // 因为这里加了synchronized关键字，就相当于是退化成了串行执行。
-            synchronized (SingletonDemo.class) {
+            synchronized (SingletonDemo.class) { // 如果把锁直接放到外边，把第一次判断，去掉的话，这样每次调用方法获取对象时，都会加锁，这样也会影响性能。
                 // 注意：synchronized修饰的变量不能为null
                 // 第二次检查是为了保证多个线程同时通过了第一次检查
                 if (null == singletonDemo) {
