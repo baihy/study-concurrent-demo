ef5b1f73b5560135dc552ebd2845a4f835cc4e1d
huayang.bai <1053091115@qq.com>
2019-08-12 15:42:36 +0800

    并发编程学习demo代码

--- cas-demo/src/main/java/com/baihy/ThreadDemo.java
+++ cas-demo/src/main/java/com/baihy/ThreadDemo.java
@@ -25,6 +25,7 @@ public class ThreadDemo {
         thread.start();
         try {
             thread.join(); // 方法的含义就是把Thread线程加入到main线程中，等到thread线程执行完毕之后，才会继续执行main线程
+            // join()相当于是线程的执行是有序的。就是线程被暂停，有序的执行其他的线程。
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
