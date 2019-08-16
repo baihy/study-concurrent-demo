0cd1de1e87d9f66e08555d0777db6f43715bbbad
huayang.bai <1053091115@qq.com>
2019-08-16 18:02:34 +0800

    添加了自定义锁

---
+++ concurrent-collection-demo/src/main/java/com/baihy/collection/HashMapDemo.java
@@ -0,0 +1,21 @@
+package com.baihy.collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @projectName: study-concurrent-demo
+ * @packageName: com.baihy.collection
+ * @description:
+ * @author: huayang.bai
+ * @date: 2019/08/16 16:16
+ */
+public class HashMapDemo {
+
+     public static void main(String[] args){
+
+         ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+
+
+     }
+
+}
