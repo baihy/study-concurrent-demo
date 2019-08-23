a570c94b4bf7bff5913272fd4bdbd76043548c7b
huayang.bai <1053091115@qq.com>
2019-08-23 14:58:35 +0800

    添加了线程池状态的测试

---
+++ concurrent-collection-demo/src/main/java/com/baihy/collection/Java8StreamAPIDemo.java
@@ -0,0 +1,19 @@
+package com.baihy.collection;
+
+/**
+ * @projectName: study-concurrent-demo
+ * @packageName: com.baihy.collection
+ * @description:
+ * @author: huayang.bai
+ * @date: 2019/08/23 14:54
+ */
+public class Java8StreamAPIDemo {
+
+    public static void main(String[] args){
+
+        //待测
+
+    }
+
+
+}
